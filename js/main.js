8cc0bb67b56825bf97ac9a775be6e88a36607999
Pachi <pachicodes@gmail.com>
2020-01-16 15:46:06 -0500

    modall add to all the lines

--- js/main.js
+++ js/main.js
@@ -20,23 +20,19 @@ let trans = () => {
 };
 
 //Modal
-var modal = document.getElementById("Modal");
-var line = document.getElementById("modalLine");
+var modal = document.querySelector("[data-modal]");
+var line = document.querySelector("[data-line]");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("modal__close")[0];
 
-// When the user clicks on the button, open the modal
 line.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
